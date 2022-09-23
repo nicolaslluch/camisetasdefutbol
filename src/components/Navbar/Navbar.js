3e783dd61930c743913ac933e00afd65c8008d44
nicolas_lluch <nicolaslluch17@gmail.com>
2022-09-22 21:20:15 -0300

    segundo commit primera entrega Lluch

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,16 +3,16 @@ import CartWidget from '../Cartwidget/CartWidget'
 
 const Navbar = () => {
     return(
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class= "navbar-expand-lg navbar">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Camisetas</a>
     <button class="navbar-toggler boton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active textonav" aria-current="page" href="#">Inicio</a>
+          <a class="nav-link textonav" aria-current="page" href="#">Inicio</a>
         </li>
         <li class="nav-item">
           <a class="nav-link textonav" href="#">Nosotros</a>
--- src/components/Pages/ItemListContainer.js
+++ src/components/Pages/ItemListContainer.js
@@ -3,7 +3,7 @@ import './itemlistcontainer.css';
 const ItemListContainer = (props) => {
     return(
         <div className="d-grid gap-2 col-6 mx-auto">
-            <h1 className='btn btn-lg m-1 mt-3 p-4 saludo'>{props.greeting} </h1>
+            <h1 className='btn btn-lg m-1 mt-3 p-4 greeting'>{props.greeting} </h1>
         </div>
         )
 }
